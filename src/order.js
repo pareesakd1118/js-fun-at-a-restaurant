97bca1817bf21effefe8d7b632c43194475b1db9
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2023-12-01 19:47:32 -0500

    Pareesa edits

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+//1------------------------------------------------------------------
+function takeOrder(orderNum, array){
+  if (array.includes(orderNum) === false && array.length < 3) {
+    array.push(orderNum) }
+    return array
+}
+//2------------------------------------------------------------------
+function refundOrder(orderNum, array){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].orderNumber === orderNum) {
+      array.splice(i,1)
+    }
+  }
+}
+//3----------------------------------------------------------------------
+
+function listItems(array){
+  var listItems = []
+  for (var i = 0; i < array.length; i++){
+    listItems.push(array[i].item)
+
+  }
+return listItems.join(", ")
+}
+
+//4----------------------------------------------------------------------
+
+function searchOrder(array, item){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].item === item) {
+      return true
+}  
+  }
+  return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+takeOrder,
+refundOrder,
+listItems,
+searchOrder
 }
