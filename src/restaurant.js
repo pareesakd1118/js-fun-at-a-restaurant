2bfb12563dfbcbb4275fd3a123ed34c725ac88d3
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2023-12-03 19:38:06 -0800

    Pareesa final attempt question 3

--- src/restaurant.js
+++ src/restaurant.js
@@ -25,18 +25,32 @@ function addMenuItem(name, obj){
   }
 }
 //3------------------------------------------------------------------------
-function removeMenuItem(name, obj, type){
-  if (name.menus.breakfast.includes(obj) === false && name.menus.lunch.includes(obj) === false && name.menus.dinner.includes(obj) === false){
-    if (obj.type === "lunch") {
-    return name.menus.lunch.push(obj)
-   } else if (obj.type === "breakfast") {
-     return name.menus.breakfast.push(obj)
-   } else {
-     return name.menus.dinner.push(obj)
-   }
-     }
-
-     else
+function removeMenuItem(name, food, type){
+  for (var i = 0; i < name.menus.breakfast.length; i++) {
+    if (name.menus.breakfast[i].name = food) {
+       var includes = true
+      return `No one is eating our ${food} - it has been removed from the ${type} menu`
+    }
+  }
+  
+  if (!includes) {
+    for (var i = 0; i < name.menus.lunch.length; i++) {
+      if (name.menus.lunch[i].name = food) {
+        includes = true
+        return `No one is eating our ${food} - it has been removed from the ${type} menu`
+      }
+    }
+  }
+  
+  if (!includes) {
+    for (var i = 0; i < name.menus.dinner.length; i++) {
+      if (name.menus.dinner[i].name = food) {
+        includes = true
+        return `No one is eating our ${food} - it has been removed from the ${type} menu`
+      }
+    }
+  } 
+ 
      
 }
 
@@ -72,7 +86,7 @@ if (!includes) {
   return `Sorry, we aren't serving ${foodItem.name} today.`
 }
 }
-//---------------
+
 
 
 module.exports = {
